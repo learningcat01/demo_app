4e0ed1d6bbcd94d9fbd97dbe91a24df041548ef9
Learning Cat <pc@smyrno.(none)>
2012-10-03 08:22:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '42ab39423c3c5d7459818f3913d4f3cb8c134bc318229ab791c980551fa7d072a53101835a0b9ad376e8662d911a753e5a09776c4928d311ab4f18f7c3869080'
